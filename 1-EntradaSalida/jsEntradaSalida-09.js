87d8e2618d0d0b4b7bafaf43122b7a808228cd3e
ornecurcio <ornecurcio@gmail.com>
2021-02-04 20:06:42 -0300

    Ejercicios de entrada salida del 1 al 10

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,10 +1,21 @@
-/*
+/* Ornela Ivana Curcio
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo; 
+	let aumento1; 
+	let resultado;
 
-	alert("ok");
+sueldo = document.getElementById("txtIdSueldo").value; 
+sueldo = parseInt(sueldo); 
+
+aumento1 = sueldo / 10; 
+aumento1 = parseInt(aumento1)
+
+resultado = sueldo + aumento1; 
+
+document.getElementById("txtIdResultado").value = resultado	;
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -1,9 +1,22 @@
-/*
+/*Ornela Curcio
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
-{
-	alert("ok");
+{ 
+	let importe; 
+	let descuento; 
+	let resultado; 
+
+	importe = document.getElementById("txtIdImporte").value; 
+	importe = parseInt(importe); 
+
+	descuento = importe / 4; 
+	descuento = parseInt(descuento); 
+
+	resultado = importe - descuento; 
+
+	document.getElementById("txtIdResultado").value = resultado; 
+
 }
