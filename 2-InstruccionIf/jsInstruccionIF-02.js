c3ad84da430591c2287def289b240432f3812a30
ornecurcio <ornecurcio@gmail.com>
2021-02-05 19:54:07 -0300

    Instruccion 2

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,6 +1,20 @@
 function mostrar()
 {
 	//tomo la edad  
+	let edad; 
+
+	edad = document.getElementById("txtIdEdad").value; 
+	edad = parseInt(edad); 
+	
+	if(edad>17)
+	{
+		alert("La persona es mayor de edad")
+	}
+
+	
+	
+	
+	
 	alert("ok");
 	
 
