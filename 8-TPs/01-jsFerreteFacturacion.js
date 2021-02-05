d24ef603f13bd79672b231b918e90ccf46f7c400
ornecurcio <ornecurcio@gmail.com>
2021-02-04 21:54:21 -0300

    Ejercicios de TP 1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,74 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    let precioUno; 
+    let precioDos; 
+    let precioTres; 
+    let resultado; 
+
+    precioUno = document.getElementById("txtIdPrecioUno").value; 
+    precioUno = parseInt(precioUno); 
+
+    precioDos = document.getElementById("txtIdPrecioDos").value; 
+    precioDos = parseInt(precioDos); 
+
+    precioTres = document.getElementById("txtIdPrecioTres").value; 
+    precioTres = parseInt(precioTres); 
+
+    resultado = precioUno + precioDos + precioTres; 
+
+    console.log(resultado); 
+    alert(resultado); 
+
+
 }
 function Promedio () 
 {
-	
+	let precioUno; 
+    let precioDos; 
+    let precioTres; 
+    let resultado; 
+    let promedio
+
+    precioUno = document.getElementById("txtIdPrecioUno").value; 
+    precioUno = parseInt(precioUno); 
+
+    precioDos = document.getElementById("txtIdPrecioDos").value; 
+    precioDos = parseInt(precioDos); 
+
+    precioTres = document.getElementById("txtIdPrecioTres").value; 
+    precioTres = parseInt(precioTres); 
+
+    resultado = precioUno + precioDos + precioTres; 
+
+    promedio = resultado / 3; 
+
+    console.log(promedio); 
+    alert(promedio); 
 }
 function PrecioFinal () 
 {
-	
+	let precioUno; 
+    let precioDos; 
+    let precioTres; 
+    let resultado; 
+    let resultadoIva; 
+    let resultadoFinal
+
+    precioUno = document.getElementById("txtIdPrecioUno").value; 
+    precioUno = parseInt(precioUno); 
+
+    precioDos = document.getElementById("txtIdPrecioDos").value; 
+    precioDos = parseInt(precioDos); 
+
+    precioTres = document.getElementById("txtIdPrecioTres").value; 
+    precioTres = parseInt(precioTres); 
+
+    resultado = precioUno + precioDos + precioTres; 
+  
+    resultadoIva = resultado * 0.21; 
+
+    resultadoFinal = resultado + resultadoIva; 
+
+    alert(resultadoFinal); 
 }
