aa1f08dc60eed18c73b87ce12edc5650b1cdda66
ornecurcio <ornecurcio@gmail.com>
2021-02-05 20:24:16 -0300

    Instruccion 3

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -4,7 +4,7 @@ Al ingresar una edad debemos informar si la persona es mayor de edad,
  sino informar que es un menor de edad*/
 
 function mostrar()
-{
+{  // INICIO DE FUNCION
 	//tomo la edad  
 	let edad; 
 
@@ -15,10 +15,31 @@ function mostrar()
 	{
 		alert("La persona es mayor de edad")
 	}
-    
-	if(edad<18)
+	else
 	{
 		alert("La persona es menor de edad")
 	}
 
-}//FIN DE LA FUNCIÓN
+	// por cada else, hay un solo if, pero puede haber mas de un if 
+
+	/*if(edad<18)
+	{
+		alert("La persona es menor de edad")
+	}*/
+
+	/* otra solucion es: 
+
+	let edad; 
+
+	edad = document.getElementById("txtIdEdad").value; 
+	edad = parseInt(edad); 
+	
+	if(edad>17)
+	{
+		alert("La persona es mayor de edad")
+	}
+	alert("La persona es menor de edad")//
+	aunque aca lo ejecuta igual aunq no ponga edad,
+	 funciona, no es lo correcto  */
+
+} // FIN DE LA FUNCION
