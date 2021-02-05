9391485de40ff0cd1ba1a6e36824cce0086e058d
ornecurcio <ornecurcio@gmail.com>
2021-02-05 20:05:31 -0300

    Instruccion 3

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,24 @@
+
+/*Ornela Curcio 
+Al ingresar una edad debemos informar si la persona es mayor de edad,
+ sino informar que es un menor de edad*/
+
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	let edad; 
+
+	edad = document.getElementById("txtIdEdad").value; 
+	edad = parseInt(edad); 
+	
+	if(edad>17)
+	{
+		alert("La persona es mayor de edad")
+	}
+    
+	if(edad<18)
+	{
+		alert("La persona es menor de edad")
+	}
 
 }//FIN DE LA FUNCIÓN
