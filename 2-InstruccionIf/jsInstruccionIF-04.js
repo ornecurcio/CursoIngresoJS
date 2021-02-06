c5146cf471c272488f9aa1548f9a803eddf25962
ornecurcio <ornecurcio@gmail.com>
2021-02-05 21:01:22 -0300

    Instruccion 4

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,22 @@
+/* Ornela Curcio
+Al ingresar una edad debemos informar si la persona es adolescente, 
+edad entre 13 y 17 años (inclusive) .*/
 function mostrar()
-{
+{ 
 	//tomo la edad  
-	alert("ok");
+	let edad; 
 
+	edad = document.getElementById("txtIdEdad").value; 
+	edad = parseInt(edad); 
+	
+	if(edad<18)
+	{
+	  if(edad>12)
+	  {
+		alert("Usted es adolescente")
+	  }
+	 
+	}
 
+	
 }//FIN DE LA FUNCIÓN
