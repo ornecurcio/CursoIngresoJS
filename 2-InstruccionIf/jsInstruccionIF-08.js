d51b2a6b893977ff110c1fa7d413b75d2481b660
ornecurcio <ornecurcio@gmail.com>
2021-02-06 19:02:52 -0300

    Instruccion 8

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,7 +1,12 @@
+/*Curcio Ornela 
+Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", 
+NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, 
+mostrar el siguiente mensaje: 'Es soltero y no es menor.'*/
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	// <18 y !=Soltero nada 
+	// >18 
 	
 
 
