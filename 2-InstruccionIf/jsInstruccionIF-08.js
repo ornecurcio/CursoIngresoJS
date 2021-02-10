9ceafe19eb8627abb8439fc9982d321d03e134ca
ornecurcio <ornecurcio@gmail.com>
2021-02-10 14:29:47 -0300

    Instruccion 8

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -8,6 +8,22 @@ function mostrar()
 	// <18 y !=Soltero nada 
 	// >18 
 	
+	let estado;  
+	let edad; 
+	
+	
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad); 
 
+	estado = document.getElementById("estadoCivil").value;
+
+	/*if(edad<18 && estado!="Soltero")
+	{
+		console.log("NADA")
+	}*/
+	if(edad>17 && estado=="Soltero")
+	{
+		alert("Es soltero y no es menor")
+	}
 
 }//FIN DE LA FUNCIÓN
