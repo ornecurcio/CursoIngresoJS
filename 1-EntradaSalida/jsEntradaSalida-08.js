d0cb9b13290a7e5a97229f85890cf1996b4fc076
ornecurcio <ornecurcio@gmail.com>
2021-02-10 17:11:15 -0300

    Tarea Entrada salida 3

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -3,7 +3,7 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
-function SacarResto()
+/*function SacarResto()
 {
 	let NumeroDividendo;
 	let NumeroDivisor;
@@ -18,5 +18,31 @@ function SacarResto()
 	resto = NumeroDividendo % NumeroDivisor
 
 	alert("El resto es " + resto); 
+
+}*/
+
+/* Ornela Curcio 
+3 - Ingresar dos numeros por id
+Se pide:
+mostrar por console.log:
+a) La suma de los dos numeros
+b) El promedio de los numeros.
+c) El modulo de los numeros (el primero en modulo del segundo)*/
+
+function sumar()
+{
+ let numeroUno; 
+ let numeroDos; 
+ let resultado; 
+
+ numeroUno = document.getElementById("txtIdNumeroDividendo").value; 
+ numeroUno = parseInt(numeroUno); 
+
+ numeroDos = document.getElementById("txtIdNumeroDivisor").value; 
+ numeroDos = parseInt(numeroDos); 
+
+ resultado = numeroUno + numeroDos; 
+
+ console.log(resultado); 
 
 }
