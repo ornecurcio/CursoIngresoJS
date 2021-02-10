0435af32fab7660f5a931a22c2145fb26fc43806
ornecurcio <ornecurcio@gmail.com>
2021-02-10 15:02:23 -0300

    Instruccion 9

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,14 @@
+/*Ornela Curcio
+Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive*/
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	let numero; 
+	let maximo = 10; 
+	let minimo = 1; 
+	
+	numero = Math.round(Math.random () *(maximo - minimo) + minimo); 
+			//round redondea mas cercano entero >.5 para arria y <.49 para abajo
+			// el ramdom solo lo hace entre 0 y 1
+	alert(numero); 
 
 }//FIN DE LA FUNCIÓN
