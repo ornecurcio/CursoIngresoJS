4f2d018d4e30d9543ff0a6917bd55b97c35e6cee
ornecurcio <ornecurcio@gmail.com>
2021-02-10 15:22:43 -0300

    Instruccion 10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,32 @@
+/* Ornela Curcio 
+Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4*/
+
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota; 
+	nota = Math.round(Math.random () *9 + 1); 
+	
+	//Excelente 9||10
+	//Aprobo >3 
+	//Vamos,,, <4
 
+	if(nota>3)
+	{
+	  if(nota==10 || nota==9)
+	  {
+		  alert(nota + " EXECELENTE");
+	  }
+	  else
+	  {
+          alert(nota + " APROBÓ");
+	  }
+	}
+	else
+	{
+          alert(nota + " Vamos, la proxima se puede");
+	}
 }//FIN DE LA FUNCIÓN
