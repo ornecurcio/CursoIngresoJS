574d7bb2e911115c07da57dc6278397ba3d0492f
ornecurcio <ornecurcio@gmail.com>
2021-02-11 18:49:33 -0300

    Switch 2

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,9 +1,35 @@
+/*al seleccionar un mes informar.
+si estamos en Invierno: "Abrigate que hace frio."
+si aún no llego el Invierno: "Falta para el invierno."
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!."
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.*/
+
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes; 
+	let mensaje;
+     
+	mes = document.getElementById("txtIdMes").value; 
+	
+	if(mes=="Julio" || mes=="Agosto")
+	{
+		mensaje = "Abrigate que hace frio"; 
+	}
+	else if(mes=="Septiembre" || mes== "Octubre" || mes=="Noviembre" || mes== "Diciembre")   
+	{
+		mensaje = "Ya pasamos el frio, ahora calor!"; 
+	}
+    else
+	{
+		mensaje = "Falta para el invierno"; 
+	}	
+	
+	alert(mensaje); 
 
+	/*switch(mes)
+	{
+		case
+	}*/
 
 
 
