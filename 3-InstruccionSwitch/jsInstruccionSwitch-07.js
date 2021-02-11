d7de52a2b2bb949252eb6cd309f15a6166a0047b
ornecurcio <ornecurcio@gmail.com>
2021-02-11 20:26:07 -0300

    Switch 7

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,29 @@
+/*Ornela Curcio
+Al selecionar un destino , indicar el punto 
+cardinal de nuestro pais en donde se encuentra Norte,
+ Sur, Este u Oeste*/
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino; 
+	let mensaje; 
 
+	destino = document.getElementById("txtIdDestino").value; 
+
+	switch(destino)
+	{
+      case"Bariloche": 
+	       mensaje = "Este"; 
+		   break; 
+	  case "Cataratas":
+		   mensaje = "Norte"; 
+		   break;  
+	  case "Ushuaia":
+	       mensaje = "Sur";  
+		   break; 
+	  case "Mar del plata": 
+	       mensaje = "Oeste"; 
+		   break; 
+	}
+
+	alert(mensaje); 
 }//FIN DE LA FUNCIÓN
