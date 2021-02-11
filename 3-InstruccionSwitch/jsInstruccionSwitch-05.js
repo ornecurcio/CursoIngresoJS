7da79649749ebf4a20a461b03a031551c0360e5e
ornecurcio <ornecurcio@gmail.com>
2021-02-11 19:57:02 -0300

    Switch 5

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,9 +1,26 @@
+/* Ornela Curcio
+Al ingresar una hora, informar:
+si está entre las 7 y las 11 : "Es de mañana.".*/
+
 function mostrar()
 {
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
-	
+	let hora; 
+
+	hora = document.getElementById("txtIdHora").value; 
+
+	switch(hora)
+	{
+		case "7":
+		case "8": 
+		case "9": 
+		case "10": 
+		case "11": 
+		   alert("Es de mañana"); 
+		   break; 
+		default:
+		   console.log("Nada");  
+		   break; 
+	}
 	
 
 
