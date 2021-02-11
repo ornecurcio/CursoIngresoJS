8760b5cf387e088aae5873dee7cd0ee71c73bb04
ornecurcio <ornecurcio@gmail.com>
2021-02-11 19:42:41 -0300

    Switch 4

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,10 +1,36 @@
+
+/*Ornela Curcio 
+al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.*/
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes; 
+	let mensaje; 
+
+	mes = document.getElementById("txtIdMes").value;
+
+	switch(mes)
+	{
+      case"Febrero": 
+	       mensaje = "Tiene 28 días"; 
+		   break; 
+	  case"Enero": 
+	  case"Marzo": 
+	  case"Mayo": 
+	  case"Julio": 
+	  case"Agosto": 
+	  case"Octubre":
+	  case"Diciembre": 
+	       mensaje = "Tiene 31 días"; 
+		   break; 
+	  default: 
+	       mensaje = "Tiene 30 días"; 
+		   break; 
+	}
 	
-	
+	alert(mensaje); 
 
 
 
