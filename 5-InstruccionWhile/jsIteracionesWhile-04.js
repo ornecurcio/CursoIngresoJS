83a6d417a255b58da988aa81917c96ef6104a658
ornecurcio <ornecurcio@gmail.com>
2021-02-12 20:29:20 -0300

    While 4

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -7,3 +7,10 @@ function mostrar()
 	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
 	
 }//FIN DE LA FUNCIÓN
+
+let numero; 
+	numero = prompt("Ingrese numero entre 0 y 9 "); 
+	while(numero<0 || numero>9)
+	{
+        numero=prompt("error, ingrese numero entre 0 y 9"); 
+	}
