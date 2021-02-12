1c55dcea3053e044276ca9a0eb99e532d52ed8ec
ornecurcio <ornecurcio@gmail.com>
2021-02-12 20:17:36 -0300

    While 1

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -1,7 +1,16 @@
-/*
+/* Ornela Curcio 
 al presionar el botón mostrar 10 repeticiones 
 con números ASCENDENTE, desde el 1 al 10.*/
+
 function mostrar()
 {
-	alert('iteración while');
+	let contador; 
+
+	contador = 0
+
+	while(contador<10)
+	{
+       contador = contador + 1; 
+	   alert(contador); 
+	}
 }//FIN DE LA FUNCIÓN
