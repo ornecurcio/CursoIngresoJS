080819727a22ebd718cf124a3c66a60605150b06
ornecurcio <ornecurcio@gmail.com>
2021-02-11 22:20:20 -0300

    Switch 10

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,61 @@
-function mostrar()
+/* Ornela Curcio 
+Enunciado:
+una agencia de viajes nos piden informar si hacemos viajes a lugares según la 
+estación del año estemos, informar si "Se viaja" o "No se viaja" a ese lugar
+en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
+en Verano: Se viaja a Mar del plata y Cataratas solamente
+en Otoño: Se viaja a todos los destinos.
+primavera: solo no se viaja a Bariloche
+*/function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacion; 
+	let lugar; 
+	let mensaje; 
 
+	estacion = document.getElementById("txtIdEstacion").value; 
+	lugar = document.getElementById("txtIdDestino").value;  
+
+	switch(estacion)
+	{
+		case"Invierno": 
+		  switch(lugar)
+		  {
+			  case"Bariloche": 
+			       mensaje = "Se viaja"; 
+			       break; 
+			  default: 
+			       mensaje = "No se viaja"; 
+				   break;  
+		  }break;
+		case"Verano": 
+		   switch(lugar)
+		   {
+			   case"Mar del plata": 
+			   case "Cataratas": 
+			       mensaje = "Se viaja"; 
+				   break; 
+			   default: 
+			       mensaje = "No se viaja"; 
+				   break; 
+		   }break;
+		case"Otoño": 
+		    switch(lugar)
+			{
+				default: 
+				mensaje = "Se viaja"; 
+				break; 
+			}break; 
+		case"Primavera": 
+		    switch(lugar)
+			{
+				case"Bariloche":
+				     mensaje = "No se viaja"; 
+					 break; 
+				default: 
+				     mensaje = "Se viaja"; 
+					 break; 	 
+			}break; 
+	}
+	alert(mensaje); 
+	
 }//FIN DE LA FUNCIÓN
