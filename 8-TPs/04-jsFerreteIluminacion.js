dd89ce34dea3b9b627f3d56a88bb108e290be9d3
ornecurcio <ornecurcio@gmail.com>
2021-02-11 23:53:25 -0300

    Tp 4 con Switch y variantes

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -13,7 +13,9 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
- */
+ */ 
+//Ferrete iluminancion Con if (cantidad) y switch (marca)
+
 function CalcularPrecio () 
 {
      let cantidadLamparitas; 
@@ -23,13 +25,160 @@ function CalcularPrecio ()
      let ingresosBrutos; 
 
      cantidadLamparitas = document.getElementById("txtIdCantidad").value; 
-     cantidadLamparitas = parseInt(cantidadLamparitas); 
-
      precioFinal = cantidadLamparitas*35; 
      
      marca = document.getElementById("Marca").value; 
      descuento=0; 
      if(cantidadLamparitas>5)
+     {
+       descuento=50;
+     }else
+     { 
+        if(cantidadLamparitas==5)
+        {
+          switch(marca)
+          {
+             case"ArgentinaLuz":
+                  descuento=40;
+                  break; 
+             default: 
+                  descuento=30;
+                  break; 
+          }
+        }else
+         { 
+            if(cantidadLamparitas==4)
+            {
+              switch(marca) 
+              {
+                case"ArgentinaLuz":
+                case"FelipeLamparas":
+                     descuento=25; 
+                     break;
+                default: 
+                     descuento=20;
+                     break; 
+              }
+            }else
+            {
+               if(cantidadLamparitas==3)
+               {
+                 switch(marca) 
+                 {
+                  case"ArgentinaLuz":
+                      descuento=15;
+                      break;  
+                  case"FelipeLamparas":
+                      descuento=10;
+                      break; 
+                  default: 
+                       descuento=5;
+                       break; 
+                 }
+                }
+             }
+          }
+      }
+     
+     precioFinal = precioFinal - (precioFinal*descuento/100);
+     document.getElementById("txtIdprecioDescuento").value = precioFinal;
+   
+    }//FINAL DE FUNCION
+      
+     //Ferrete iluminancion Con switch(cantidad) y if (marca)
+     /*switch(cantidadLamparitas)
+     { 
+      case"5": 
+        if(marca=="ArgentinaLuz")
+        {
+          descuento = 40; 
+        }
+        else 
+        {
+          descuento = 30;
+        }break;     
+      case"4": 
+        if(marca=="ArgentinaLuz" || marca=="FelipeLamparas")
+        {
+          descuento = 25; 
+        }
+        else
+        {
+          descuento = 20;
+        }break;
+      case "3": 
+         if(marca=="ArgentinaLuz")
+         { 
+           descuento = 15; 
+         } 
+         else if(marca=="FelipeLamparas")
+         {
+           descuento = 10; 
+         } 
+          else
+          {
+            descuento = 5;
+          }break; 
+        case "1": 
+        case "2":
+            descuento = 0; 
+            break;
+        default: 
+        descuento = 50; 
+        break;     
+     }*/
+     
+     ////Ferrete iluminancion solo SWITCH (la parte des descuento)
+
+     /*switch(cantidadLamparitas)
+     { 
+       case"5": 
+        switch(marca)
+        {
+          case"ArgentinaLuz": 
+               descuento = 40; 
+               break; 
+          default: 
+               descuento = 30; 
+               break; 
+        }break; 
+        case"4": 
+         switch(marca)
+         {
+           case"ArgentinaLuz": 
+           case"FelipeLamparas": 
+               descuento = 25; 
+               break;
+           default: 
+               descuento = 20; 
+               break; 
+         }break; 
+        case "3": 
+         switch(marca)
+         {
+           case"ArgentinaLuz": 
+               descuento = 15; 
+               break; 
+           case"FelipeLamparas": 
+               descuento = 10; 
+               break; 
+           default: 
+               descuento = 5;
+               break;  
+         }break; 
+        case "1": 
+        case "2":
+            descuento = 0; 
+            break
+        default: 
+        descuento = 50; 
+        break;     
+     }
+     precioFinal = precioFinal - (precioFinal*descuento/100);
+     document.getElementById("txtIdprecioDescuento").value = precioFinal;*/
+    
+     // todo el codigo con IF
+     /*if(cantidadLamparitas>5)
      {
        descuento=50;
      }else
@@ -85,8 +234,8 @@ function CalcularPrecio ()
         alert("Usted pago "+precioFinal*10/100+" de IIBB"); 
         
      }
-     document.getElementById("txtIdprecioDescuento").value = precioFinal; 
+     document.getElementById("txtIdprecioDescuento").value = precioFinal;*/
  
-}
+
 
 //Ornela Curcio 
