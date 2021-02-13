830ff521d0c5f71d938edaedd685cac7aaa0a7da
ornecurcio <ornecurcio@gmail.com>
2021-02-12 21:56:35 -0300

    While 5

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -1,4 +1,4 @@
-/*
+/*Ornela Curcio
 Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
 function mostrar()
@@ -6,11 +6,11 @@ function mostrar()
 	var sexo;
 	sexo = prompt("ingrese f ó m ");
 
-	while(sexo!="f" || sexo!="m")
+	while(sexo!="f" && sexo!="m")
 	{
 		sexo = prompt("error ingrese f o m"); 
 	}
     
-	document.getElementById("txtIdSexo").value; 
+	document.getElementById("txtIdSexo").value = sexo; 
 
 }//FIN DE LA FUNCIÓN
