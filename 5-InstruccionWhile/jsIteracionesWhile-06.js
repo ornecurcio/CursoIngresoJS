1be0d04e3074f9e46f4bccb59f272f8b109978f2
ornecurcio <ornecurcio@gmail.com>
2021-02-12 22:09:42 -0300

    While 6

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,25 @@
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	let contador;
+	let acumulador;
+	let numeroIngresado;
+	let promedio; 
 
 	contador=0;
 	acumulador=0;
 	
+    while(contador<5)
+	{ 
+	  contador = contador + 1; 
+      numeroIngresado = prompt("Ingrese numero " + contador); 
+	  numeroIngresado = parseInt(numeroIngresado); 
+ 
+	  acumulador = acumulador + numeroIngresado; 
+
+	}
+
+	promedio = acumulador/contador; 
 	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
+	document.getElementById("txtIdSuma").value = acumulador; 
+	document.getElementById("txtIdPromedio").value = promedio; 
 }//FIN DE LA FUNCIÓN
