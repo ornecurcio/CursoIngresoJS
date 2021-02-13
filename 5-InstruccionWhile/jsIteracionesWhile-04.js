022d173cef62e193c30c7a1dea772806453a5194
ornecurcio <ornecurcio@gmail.com>
2021-02-12 21:33:44 -0300

    While 4

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,16 +1,21 @@
-/*
+/* Ornela Curcio
 al presionar el botón 
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	let numero; 
 	
+	numero = prompt("ingrese un número entre 0 y 10.");
+	
+	while(numero<0 || numero>9)
+	{
+		numero = prompt("error, ingrese numero 0 y 9"); 
+	}
 }//FIN DE LA FUNCIÓN
 
-let numero; 
+/*let numero; 
 	numero = prompt("Ingrese numero entre 0 y 9 "); 
 	while(numero<0 || numero>9)
 	{
         numero=prompt("error, ingrese numero entre 0 y 9"); 
-	}
+	}*/
