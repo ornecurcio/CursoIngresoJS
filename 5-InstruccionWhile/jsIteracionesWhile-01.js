859ec7741a1bebb59f58d84dd79debf16d928cbf
ornecurcio <ornecurcio@gmail.com>
2021-02-13 00:27:12 -0300

    While 1

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -4,13 +4,13 @@ con números ASCENDENTE, desde el 1 al 10.*/
 
 function mostrar()
 {
-	let contador; 
+	let contador; // declaracion e inicializacion
 
 	contador = 0
 
-	while(contador<10)
+	while(contador<10) // variable de control (osea la condicion para que se ejecute)
 	{
-       contador = contador + 1; 
-	   alert(contador); 
+       contador = contador + 1; // modificacion de la variable de control, 
+	   alert(contador); // en principio la modificacion debe existir para salir del bucle. 
 	}
 }//FIN DE LA FUNCIÓN
