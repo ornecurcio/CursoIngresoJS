dfdc888a6ccb5cc9b46f2b661e82b78c3abcd950
ornecurcio <ornecurcio@gmail.com>
2021-02-14 14:18:33 -0300

    While 6

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -9,11 +9,11 @@ function mostrar()
 	acumulador=0;
 	
     while(contador<5)// esto van a ser 5 vueltas, porq inicialice en 0
-	{ 
+	{ //un contador es una variable, q es esa variable + un valor literal(1)
 	  contador = contador + 1; //esto hace correr las 5 vueltas. 
       numeroIngresado = prompt("Ingrese numero " + contador); 
 	  numeroIngresado = parseInt(numeroIngresado); 
- 
+      //el acumulador es una varible, q es esa varible + otra varible(numeroIngresado)
 	  acumulador = acumulador + numeroIngresado; //esto va a sumar los numeros q ingrese
 
 	}
