5cb876ff489463e751cc7e4554f7ebc54b6ca1f3
ornecurcio <ornecurcio@gmail.com>
2021-02-13 23:12:21 -0300

    While 7

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -4,15 +4,36 @@ e informar la suma acumulada y el promedio.
 */
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var respuesta;
+  
+	let contador;
+	let acumulador;
+	let respuesta;
 	contador=0;
 	acumulador=0;
 	respuesta='si';
 
+    while(respuesta == 'si')
+	{
+	  contador = contador + 1; 
+      numeroIngresado = prompt("Ingrese numero " + contador); 
+	  numeroIngresado = parseInt(numeroIngresado); 
+ 
+	  acumulador = acumulador + numeroIngresado; 
+	  respuesta = prompt("¿Quiere ingresar otro numero?"); 
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
-
+	}
+	document.getElementById("txtIdSuma").value=acumulador;
+	document.getElementById("txtIdPromedio").value=acumulador/contador; // promedio
+    
 }//FIN DE LA FUNCIÓN
+
+ /* let seguir; 
+     
+    seguir = "s";  //aca estoy inicializando la variable. 
+	while(seguir == "s")
+	{
+		alert("Hola"); 
+		seguir = prompt("lo saludo de nuevo? "); // aca estoy haciendo la condicion de cambio 
+	}
+
+	  alert("ya no lo saludo"); */
