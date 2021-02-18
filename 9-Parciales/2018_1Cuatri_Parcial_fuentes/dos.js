504f5ea6fffe66896ddca32de9d7d7177fb06c77
ornecurcio <ornecurcio@gmail.com>
2021-02-18 18:40:28 -0300

    parcial 2018 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,15 @@
+/*Ornela Curcio 
+Bienvenidos.
+mostrar por alert el mensaje "usted es xxxxx y vive en la localidad de xxxxxxxx".
+*/
 function mostrar()
 {
-  
+  let nombre; 
+  let localidad; 
+
+  nombre = document.getElementById("elNombre").value; 
+
+  localidad = document.getElementById("laLocalidad").value; 
+
+  alert("Usted es "+nombre+" y vive en la localidad de "+localidad); 
 }
