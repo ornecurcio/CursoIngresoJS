c86b3d1025a15c0d679a19d3889e14cfa52043d8
ornecurcio <ornecurcio@gmail.com>
2021-02-18 19:02:33 -0300

    parcial 2018 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,30 @@
+/* Ornela Curcio
+Bienvenidos.
+Pedir por prompt el precio y el porcentaje de descuento, mostrar el 
+precio final con descuento por id.*/
 function mostrar()
 {
+let precioIngresado; 
+let descuentoIngresado; 
+let precioFinal; 
 
+precioIngresado = prompt("Ingrese precio de producto"); 
+precioIngresado = parseFloat(precioIngresado); 
+while(isNaN(precioIngresado)==true || precioIngresado<0.01)
+{
+    precioIngresado = prompt("Error, ingrese precio de producto"); 
+    precioIngresado = parseFloat(precioIngresado);  
+}
+
+descuentoIngresado = prompt("Ingrese descuento desceado"); 
+descuentoIngresado = parseFloat(descuentoIngresado); 
+while(isNaN(descuentoIngresado)==true || descuentoIngresado<1 || descuentoIngresado>99)
+{
+    descuentoIngresado = prompt("Error, ingrese descuento de producto"); 
+    descuentoIngresado = parseFloat(precioIngresado);  
+}
+
+precioFinal = precioIngresado - precioIngresado*descuentoIngresado/100; 
+
+document.getElementById("elPrecioFinal").value = precioFinal; 
 }
