da2af04d50c51d5c2c9bbf2405fc87eda46d32e3
ornecurcio <ornecurcio@gmail.com>
2021-02-20 10:57:15 -0300

    tarea while sabado

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,7 +1,7 @@
 /*Ornela Curcio
 al presionar el botón mostrar 10 repeticiones 
 con números DESCENDENTES, desde el 10 al 1.*/
-function mostrar()
+/*function mostrar()
 {
 	let contador; 
 
@@ -13,4 +13,38 @@ function mostrar()
 		contador = contador - 1;
 	}
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN*/
+
+
+
+/*Ornela Curcio 
+1. Ingresar 5 numeros, y determinar la cantidad de numeros que son mayores que 10 y menores 20 (incluisive en ambos casos)*/
+function mostrar()
+{
+   let numeroIngresado; 
+   let contadorNumero;  
+   let mensaje; 
+   let contador; 
+
+   contador = 0;
+   contadorNumero = 0;  
+
+   while(contador<5)
+   {
+	   numeroIngresado = prompt("Ingrese un numero"); 
+	   numeroIngresado = parseInt(numeroIngresado); 
+	   while(isNaN(numeroIngresado)==true)
+	   {
+			numeroIngresado = prompt("Error, ingrese un numero"); 
+			numeroIngresado = parseInt(numeroIngresado);
+	   }
+
+	   if(numeroIngresado<21 && numeroIngresado>9)
+	   {
+         contadorNumero = contadorNumero + 1;
+	   }
+     contador = contador + 1; 
+	}
+    console.log("La cantidad de numeros es: "+contadorNumero); 
+	
+}
--- 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
@@ -1,4 +1,15 @@
+/*Realizar el algoritmo que permita ingresar los datos de una compra de productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+al terminar la compra el cliente accede a un descuento segun las bolsas en total
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo ("arena";"cal";"cemento") con mas cantidad de bolsas en el total de la compra.
+f) El tipo mas caro*/
 function mostrar()
 {
-  alert("dos");
+
 }
