ae6613cdfbc4646beeb6989a80f210f27653cfb9
ornecurcio <ornecurcio@gmail.com>
2021-02-20 12:23:06 -0300

    tarea while sabado

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -19,7 +19,7 @@ con números DESCENDENTES, desde el 10 al 1.*/
 
 /*Ornela Curcio 
 1. Ingresar 5 numeros, y determinar la cantidad de numeros que son mayores que 10 y menores 20 (incluisive en ambos casos)*/
-function mostrar()
+/*function mostrar()
 {
    let numeroIngresado; 
    let contadorNumero;  
@@ -53,4 +53,49 @@ function mostrar()
 	}
     console.log("La cantidad de numeros es: "+contadorNumero); 
 	
+}*/
+
+
+
+/*Ornela Curcio 
+2. Mismo ejercicio, pero no se cuantos numeros se ingresan. (Lo decide el usuario). 
+Mostrar tambien el promedio de los numeros que cumplen la condicion.*/
+function mostrar()
+{
+   let numeroIngresado; 
+   let contadorNumero;  
+   let sumadorNumero; 
+   let promedio; 
+   let respuesta; 
+
+   respuesta = "si";
+   contadorNumero = 0;
+   sumadorNumero = 0;   
+
+   while(respuesta=="si")
+   {
+	   numeroIngresado = prompt("Ingrese un numero"); 
+	   numeroIngresado = parseInt(numeroIngresado); 
+	   while(isNaN(numeroIngresado)==true)
+	   {
+			numeroIngresado = prompt("Error, ingrese un numero"); 
+			numeroIngresado = parseInt(numeroIngresado);
+	   }
+
+	   if(numeroIngresado>9 && numeroIngresado<20)
+	   {
+			 contadorNumero = contadorNumero + 1; 
+			 sumadorNumero = sumadorNumero + numeroIngresado;  
+	   }
+	   /*if(numeroIngresado>9)
+	   {
+		   if(numeroIngresado<20)
+		   {
+			   
+		   }
+	   }*/ 
+     respuesta = prompt("Desea ingresar nuevo numero?");
+	 promedio = sumadorNumero/contadorNumero; 
+	}
+    console.log("La cantidad de numeros es: "+contadorNumero+" y el promedio de la suma de esos numeros es: "+promedio); 
 }
