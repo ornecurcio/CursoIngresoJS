2a2ad77f3ac4885aa1e0ce3807fac8c7f5cd805d
ornecurcio <ornecurcio@gmail.com>
2021-02-24 20:10:41 -0300

    For 2 y 3

--- 6-InstruccionFor/jsIteracionesFor-02.js
+++ 6-InstruccionFor/jsIteracionesFor-02.js
@@ -1,5 +1,11 @@
+/*al presionar el botón mostrar 10 repeticiones con números , desde el 10 al 1.*/
 function mostrar()
 {
 
-	alert("ok");
+	let contador;
+
+  for(contador=10;contador>0;contador=contador-1)
+  {
+        console.log(contador); 
+  }
 }
--- 6-InstruccionFor/jsIteracionesFor-03.js
+++ 6-InstruccionFor/jsIteracionesFor-03.js
@@ -1,8 +1,16 @@
+/*al presionar el botón pedir la cantidad de veces que quiero repetir el mensaje "Hola UTN FRA"*/
 function mostrar()
 {
-
-	var repetciones = prompt("ingrese el número de repeticiones");
-	alert("ok");
-
+    
+	let repeticiones; 
+    let contador; 
+	
+	repeticiones = prompt("ingrese el número de repeticiones");
+	repeticiones = parseInt(repeticiones); 
+	
+	for(contador=0;contador<repeticiones;contador++)
+	{
+      console.log("Hola UTN FRA"); 
+	}
 
 }//FIN DE LA FUNCIÓN
