52a4b0bd5964375ce6a43104cf70659719171267
ornecurcio <ornecurcio@gmail.com>
2021-02-24 20:33:11 -0300

    For 5

--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -1,6 +1,21 @@
+/*
+al presionar el botón repetir el pedido de número hasta que ingresemos el 9.*/
+
 function mostrar()
 {
-	alert("ok");
+	let repeticiones; 
+	let contador; 
+    
+	repeticiones = prompt("Ingrese numero del 1 al 9")
+
+	for(contador=0;contador<15;contador++)
+	{
+       repeticiones = prompt("Ingrese nuemero")
+	   if(repeticiones=="9")
+	   {
+		   break; 
+	   }
+	}
 
 
 
