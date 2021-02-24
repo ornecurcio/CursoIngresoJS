aa7e731410cfaa3017d46f7491b2eb0b237248de
ornecurcio <ornecurcio@gmail.com>
2021-02-24 19:54:34 -0300

    For 1

--- 6-InstruccionFor/jsIteracionesFor-01.js
+++ 6-InstruccionFor/jsIteracionesFor-01.js
@@ -1,4 +1,80 @@
+/*al presionar el botón mostrar 10 repeticiones con números ASCENDENTE, desde el 1 al 10. */
+
 function mostrar()
 {
-	alert("ok");
+  let contador;
+
+  for(contador=0;contador<10;contador++)
+  {
+        console.log(contador+1); 
+  }
+
 }
+
+/*
+	var contador;
+	var respuesta;
+
+	contador=0;//1
+	while(contador<5)//2
+	{
+		console.log("la iteracion es #"+contador);
+		contador++;//3
+		//console.log("la iteracion es #"+contador);
+	}
+	// siempre sabemos el valor del contador
+
+
+
+	//contador=0;
+	for(contador=0;contador<5;contador++)
+	{
+		console.log("la iteracion es #"+(contador+30);
+		//console.log("la iteracion es #"+contador);
+	}
+	// siempre sabemos el valor del contador
+
+	respuesta="si";
+	while(respuesta=="si")
+	{
+		respuesta=prompt("desea continuar");
+	}
+
+	// no se  hace
+	for(respuesta="si";respuesta=="si";respuesta=prompt("desea continuar"))
+	{
+		
+	}
+	/*aca el continue*/
+	/*for(contador=0;contador<5 ;contador++)
+	{
+		//break;
+		console.log("la iteracion es #"+(contador+30);
+		numeroIngresado=prompt("ingrese numero");
+		numeroIngresado=parseInt(numeroIngresado);
+		while(isNaN(numeroIngresado)==true)
+		{
+			numeroIngresado=prompt("ingrese numero");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
+		continue;
+
+
+
+		if("logica necesaria")
+		{
+			//break;
+			
+		}
+		console.log("la iteracion es #"+contador);
+		edad=prompt("ingrese numero");
+		edad=parseInt(edad);
+		while(isNaN(edad)==true)
+		{
+			edad=prompt("ingrese numero");
+			edad=parseInt(edad);
+		}
+
+	}
+	//aca viene el break;*/
+
