900c3eac0c2ce58229a6a50f156f817984a6ab8e
ornecurcio <ornecurcio@gmail.com>
2021-02-24 22:21:45 -0300

    While 9

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,7 +1,7 @@
 /*Ornela Curcio
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
-function mostrar()
+/*function mostrar()
 {	// declarar variables
 	let banderaDelPrimero;//3 partes, inicializacion, validacion, cambio. 
 	let numeroIngresado;
@@ -47,5 +47,65 @@ function mostrar()
 	}
 	document.getElementById("txtIdMaximo").value = numeroMaximo;
 	document.getElementById("txtIdMinimo").value = numeroMinimo; 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN*/
+
+//Ornela Curcio while 09(el menor de los pares y el mayor de los negativos ...solo si hay)
+//Al presionar el botón pedir números hasta que el usuario quiera, 
+function mostrar()
+{
+	let numeroIngresado;
+	let menorNumeroParIngresado;
+	let mayorNumeroNegativoIngresado;
+	let respuesta; 
+	let baderaNumeroPar; 
+	let banderaNegativo; 
+
+	banderaNumeroPar = 1;
+	banderaNegativo = 1; 
+	
+    
+	for(respuesta="si";respuesta=="si";respuesta=prompt("desea continuar?"))
+	{
+        numeroIngresado=prompt(numeroIngresado); 
+		if(numeroIngresado%2==0)
+		{
+			if(banderaNumeroPar==1) 		
+			{
+				menorNumeroParIngresado = numeroIngresado; 
+				banderaNumeroPar = 0;
+			}
+			else
+			{
+				if(numeroIngresado<menorNumeroParIngresado)
+				{
+                  menorNumeroParIngresado = numeroIngresado; 
+				}
+			}	
+		}
+
+		if(numeroIngresado<0)
+		{
+			if(banderaNegativo==1) 		
+			{
+				mayorNumeroNegativoIngresado = numeroIngresado; 
+				banderaNegativo = 0; 
+			}
+			else
+			{
+				if(numeroIngresado>mayorNumeroNegativoIngresado)
+				{
+                  mayorNumeroNegativoIngresado = numeroIngresado; 
+				}
+			}	
+		}
+		
+		console.log(menorNumeroParIngresado); 
+		console.log(mayorNumeroNegativoIngresado); 
+		
+	}
+
+	
+
+  }
+  
 
