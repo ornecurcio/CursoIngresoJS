7d47b29712d9d987c7888c5d53084ef0532c1e6c
ornecurcio <ornecurcio@gmail.com>
2021-02-25 17:50:54 -0300

    For 7

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,8 @@
 /*al presionar el botón pedir un número. 
 mostrar los numeros divisores desde el 1 al número ingresado,
  y mostrar la cantidad de numeros divisores encontrados.*/
-function mostrar()
+
+ function mostrar()
 {	
 	let numeroIngresado; 
 	let contadorDivisores; 
@@ -13,15 +14,11 @@ function mostrar()
 
 	for(contador=1;contador<numeroIngresado;contador++)
 	{  
-		
 	   if(numeroIngresado%contador==0)
 	   {
 		    contadorDivisores=contadorDivisores+1;
 			console.log(contador); 
-		    
-		   console.log("total de numeros pares: "+contadorDivisores);  
-	
+	   }
 	}
-
-
+    console.log("total de numeros divisores: "+contadorDivisores);  
 }//FIN DE LA FUNCIÓN
